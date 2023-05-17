3362881c6f4f521dfeba066bc11aa718f8f8b030
Domenico De Blasi <domenico.deblasi98@gmail.com>
2023-05-17 10:36:05 +0200

    set dark theme on local storage

--- src/Context.js
+++ src/Context.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react"
+import React, { useState, useEffect, useContext } from "react"
 
 const ThemeContext = React.createContext()
 const ThemeContextUpdate = React.createContext()
@@ -26,6 +26,20 @@ export const ThemeContextProvider = ({ children }) => {
     const [darkTheme, setDarkTheme] = useState(false)
     const [isMenuOpen, setIsMenuOpen] = useState(false)
     const scrollbarWidth = window.innerWidth - document.body.clientWidth
+
+    useEffect(() => {
+        const DARK_THEME = localStorage.getItem("DARK_THEME")
+        if (DARK_THEME !== null) {
+            setDarkTheme(JSON.parse(DARK_THEME))
+        }
+    }, [])
+
+    // add darkTheme to localStorage, in order to get the right state when web page is refreshed
+    useEffect(() => {
+        console.log("DARK_THEME SET", darkTheme)
+        localStorage.setItem("DARK_THEME", JSON.stringify(darkTheme))
+    }, [darkTheme])
+
 
     const toggleTheme = () => {
         setDarkTheme(prevDarkTheme => !prevDarkTheme)
